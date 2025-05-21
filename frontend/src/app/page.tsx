34273060283f555d581f9abada63388f2ed82f1b
pioloebarle <besingapiolo@gmail.com>
2025-05-22 02:31:27 +0800

    added signup and login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,13 @@
 import { CourseCards } from "./coursePage/_components/courseCards";
-
+import Login from "./login/page";
+import SignUp from "./signup/page";
+import Link from "next/link";
 export default function Main() {
-  return (]
+  return (
     <>
-    <p>login page</p>
-    <p>tbd</p>
+    {/* <CourseCards /> */}
+    <Login />
+    {/* <SignUp /> */}
     </>
    
   );
