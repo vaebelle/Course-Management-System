e3e407191064e654aa2dee8124ec3d9076d5f1cc
pioloebarle <besingapiolo@gmail.com>
2025-05-22 00:14:39 +0800

    routing signup and login and logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { CourseCards } from "./coursePage/_components/courseCards";
-import Login from "./login/login";
-import SignUp from "./signup/signUp";
+import Login from "./login/page";
+import SignUp from "./signup/page";
 import Link from "next/link";
 export default function Main() {
   return (
     <>
     {/* <CourseCards /> */}
-    {/* <Login /> */}
-    <SignUp />
+    <Login />
+    {/* <SignUp /> */}
     </>
    
   );
